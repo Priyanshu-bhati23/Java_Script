a033c57f501a437a6afc09156eed57a7aa7e0f9f
priyanshu_bhati23 <priyanshubhati23122005@gmail.com>
2024-12-07 11:59:52 +0530

    New Commit

---
+++ Projects/BMI Cal/chaiaurcode.js
@@ -0,0 +1,25 @@
+const form =document.querySelector('form')
+//this usecase will gave you empty value
+// const height=parseInt(document.querySelector('#height').value)/
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+    const height=parseInt(document.querySelector('#height').value)
+    const weight=parseInt(document.querySelector('#weight').value)
+    const results=document.querySelector('#results')
+    if(height ===''|| height <0 || isNaN(height)){
+
+        results.innerHTML=`Please give a valid height ${height}`;
+    }else if(weight ===''|| weight <0 || isNaN(weight)){
+
+        results.innerHTML=`Please give a valid weight ${weight}`;
+    }else{
+        const BMI=(weight/((height*weight)/10000)).toFixed(2)//2 decimal number
+         //show the result 
+         results.innerHTML = `<span>${BMI}</span>`
+    }
+
+
+
+})
